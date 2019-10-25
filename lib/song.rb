c8c947d003a2e3e8825790a221734167cc83c198
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-25 16:28:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,14 @@
-require 'pry'
-
-extend Memorable::ClassMethods
-
 class Song
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
 
   def self.all
     @@songs
@@ -18,6 +16,7 @@ class Song
 
   def artist=(artist)
     @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
   end
 
 end
